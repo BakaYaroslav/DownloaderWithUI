1f42497f2724982ac6e5201daed44019edd8bd86
Yaroslav Baka <jaroslavbaka25@gmail.com>
2026-03-12 21:48:50 +0200

    Добавьте файлы проекта.

---
+++ Downloader/Downloader.cs
@@ -0,0 +1,81 @@
+﻿using System.Linq;
+using System.Windows;
+using YoutubeDLSharp;
+using YoutubeDLSharp.Options;
+
+namespace Downloader
+{
+    public class VideoDownloader
+    {
+        YoutubeDL ytdl = new YoutubeDL(); // это класс с помощью которого мы будем скачивать видео и получать информацию о виде
+        public VideoDownloader()
+        {
+            ytdl.YoutubeDLPath = @"tools\yt-dlp.exe";
+            ytdl.FFmpegPath = @"tools\ffmpeg-8.0.1-essentials_build\bin\ffmpeg.exe";
+          
+        }
+        
+        // async говорит: "этот метод будет ждать не замораживая программу"
+        public async Task Download(string url, string outputFolder, string format, IProgress<double> progress)
+        
+        {
+            ytdl.OutputFolder = outputFolder;
+            var options = new OptionSet(); // это класс, который содержит все настройки для загрузки видео.
+            options.Format = format;
+
+            var progressBar = new Progress<DownloadProgress>(p => progress.Report(p.Progress * 100));
+
+
+            var result = await ytdl.RunVideoDownload(url, overrideOptions: options, progress: progressBar);
+            if (result.Success)
+            {
+                MessageBox.Show("Video downloaded successful");
+            }
+            else
+            {
+              
+                string errors = string.Join("\n", result.ErrorOutput);
+                MessageBox.Show($"Error:\n{errors}");
+            }
+
+
+
+        }
+        public async Task<List<string>> GetVideoFormats(string url)
+        {
+            var standardQualities = new List<int> { 144, 240, 360, 480, 720, 1080, 1440, 2160 };
+            var result = await ytdl.RunVideoDataFetch(url);
+            if (!result.Success)
+            {
+                Console.WriteLine("Failed to get video info");
+                return new List<string>();
+            }
+
+            var qualities = result.Data.Formats
+                .Where(f => f.Height != null)        // только форматы у которых есть высота  .where = foreach с условием { Height: 1080, Ext: "webm" }  → true
+                .Where(f => standardQualities.Contains(f.Height.Value)) // только форматы с высотой из списка стандартных  .Where = foreach с условием 1080 → true, 123 → false
+                .Select(f => f.Height.Value)         // берём только значение высоты  .Select = это foreach который что-то вытаскивает { Height: 1080, Ext: "webm" }  →  1080
+                .Distinct()                          // убираем дубликаты // .Distinct() = 1080, 720, 1080 → 1080, 720
+                .OrderByDescending(h => h)           // сортируем от большего к меньшему
+                .ToList();
+
+            return qualities.Select(q => q.ToString()).ToList();
+        }
+
+
+
+        public async Task DownloadAudio(string url, string outputFolder, IProgress<double> progress)
+        {
+            ytdl.OutputFolder = outputFolder;
+
+
+            var progressBar = new Progress<DownloadProgress>(p => progress.Report(p.Progress * 100));
+
+
+            var result = await ytdl.RunAudioDownload(url, AudioConversionFormat.Mp3, progress: progressBar);
+
+           
+
+        }
+    }
+}
---
+++ Downloader/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Downloader
+{
+   
+    public partial class MainWindow : Window
+    {
+        VideoDownloader downloader = new VideoDownloader();
+        public MainWindow()
+        {
+            InitializeComponent();
+            qualityBox.ItemsSource = new List<string> { "1080", "720", "480", "360" };
+            qualityBox.SelectedIndex = 0;
+            typeBox.ItemsSource = new List<string> { "Video", "Audio" };
+            typeBox.SelectedIndex = 0;
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            string downFolder = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Downloads");
+            string url = urlTable.Text;
+            string type = typeBox.SelectedItem.ToString();
+            string quality = $"bestvideo[height={qualityBox.SelectedItem}]+bestaudio";
+
+            void updateProgress(double value)
+            {
+                progressBar.Value = value;
+                progressLabel.Content = $"{value:F1}%";
+            }
+
+            var progress = new Progress<double>(updateProgress);
+
+            if (type == "Video")
+            {
+                await downloader.Download(url, downFolder, quality, progress);
+            }
+            if (type == "Video")
+            {
+                try
+                {
+                    await downloader.Download(url, downFolder, quality, progress);
+                    
+                   
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error: {ex.Message}");
+                }
+            }
+            else if (type == "Audio")
+            {
+                await downloader.DownloadAudio(url, downFolder, progress);
+              
+            }
+           
+            MessageBox.Show(" Downloaded!");
+           
+
+    }
+        private void typeBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (typeBox.SelectedItem.ToString() == "Audio")
+                qualityBox.Visibility = Visibility.Collapsed;
+            else
+                qualityBox.Visibility = Visibility.Visible;
+        }
+
+
+    }
+}

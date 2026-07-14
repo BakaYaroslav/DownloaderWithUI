83e6c392d6598cf3ce1244058aeaf1884c121a02
Yaroslav Baka <jaroslavbaka25@gmail.com>
2026-07-14 12:02:46 +0300

    remove DatabaseConfig from git cheking

--- Downloader/services/DatabaseConfig.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace Downloader.services
-{
-    internal class DatabaseConfig
-    {
-        public static string ConnectionString = "server=bmshtuvbcduoktbgbqmj-mysql.services.clever-cloud.com;database=bmshtuvbcduoktbgbqmj;user=u5qmpufbrgjc1jy2;password=filbtqsvCzwnLZHSVNu6;port=3306;SslMode=Required;";
-        //public static string ConnectionString = "server=localhost;user=root;password=;database=downloader;";
-    }
-}
